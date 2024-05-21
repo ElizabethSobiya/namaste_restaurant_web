ec4fb5e9197cd195635f8454b33efdf494853cc7
ElizabethSobiya <ellizabethsobiya@gmail.com>
2024-05-21 12:48:10 +0530

    Cart chages are added

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,8 +1,29 @@
+import { useDispatch, useSelector } from "react-redux";
+import ItemList from "./ItemsList";
+import { clearCart } from "./utils/createSlice";
+
 const Cart = () => {
+  const cartItems = useSelector((store) => store.cart.items);
+  const dispatch = useDispatch((store) => store.cart.items);
+  const handleClear = () => {
+    dispatch(clearCart());
+  };
   return (
     <>
       <div className="text-center m-4 p-4">
-        <div className="text-2xl font-bold">Cart</div>
+        <h1 className="text-2xl font-bold">Cart</h1>
+        <button
+          className="bg-black text-white rounded-lg p-2"
+          onClick={() => handleClear()}
+        >
+          Clear
+        </button>
+        <div>
+          <ItemList item={cartItems} />
+          {cartItems.length === 0 && (
+            <h1>Cart is empty, add some items to the cart.</h1>
+          )}
+        </div>
       </div>
     </>
   );
--- src/components/utils/createSlice.js
+++ src/components/utils/createSlice.js
@@ -7,12 +7,14 @@ const cartSlice = createSlice({
   },
   reducers: {
     addItems: (state, action) => {
+      //redux uses Immer bts
       state.items.push(action.payload);
     },
     removeItem: (state) => {
       state.items.pop();
     },
     clearCart: (state) => {
+      // in rtk(redux toolkit) either mutate the existing state or return new state[] return {items:[]}
       state.items.length = 0;
     },
   },
