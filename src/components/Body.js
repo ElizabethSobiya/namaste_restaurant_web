f80d397cbcfa2aa054b524c76abdb0c0e9f53278
elizabeth-nh <elizabeth@nighthack.in>
2025-05-31 23:43:58 +0530

    Update index.html to improve error messaging and enhance HTML structure; modify Body component input styling for better focus visibility.

--- src/components/Body.js
+++ src/components/Body.js
@@ -96,7 +96,7 @@ function Body() {
               onKeyDown={(e) => {
                 if (e.key === "Enter") handleSearch();
               }}
-              className="pl-10 pr-4 py-2 w-full rounded-full border border-gray-400 focus:outline-none focus:ring-2 focus:ring-yellow-500 dark:bg-gray-800 dark:border-gray-600 dark:text-white"
+              className="pl-10 pr-4 py-2 w-full rounded-full border border-gray-400 focus:outline-none focus:ring-0 dark:bg-gray-800 dark:border-gray-600 dark:text-white"
             />
 
             <CiSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-600 dark:text-white" />
